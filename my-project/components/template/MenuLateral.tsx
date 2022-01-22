91fc982b91e1537e15c0ad619eec0cd991813ed9
Radames Ribeiro de Sousa <easycompvila@gmail.com>
2022-01-22 17:33:13 -0300

    Implementado incones no menu lateral

--- my-project/components/template/MenuLateral.tsx
+++ my-project/components/template/MenuLateral.tsx
@@ -1,7 +1,14 @@
+import { IconeAjustes, IconeCasa, InconeSino } from "../icons"
+import MenuItem from "./MenuItem"
+
 export default function MenuLateral(){
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuItem url="/" texto="Inicio" icone={IconeCasa}/>
+                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>
+                <MenuItem url="/novidades" texto="Novidades" icone={InconeSino}/>
+            </ul>
 
         </aside>
     )
