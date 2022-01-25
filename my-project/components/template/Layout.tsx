8a95e8aab9a1d89d6f9bbd6ecd9b51f6639bb54f
Radames Ribeiro de Sousa <easycompvila@gmail.com>
2022-01-25 19:09:44 -0300

    Criação da função alternar tema

--- my-project/components/template/Layout.tsx
+++ my-project/components/template/Layout.tsx
@@ -1,3 +1,4 @@
+import useAppData from "../../data/hook/useAppData";
 import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 import MenuLateral from "./MenuLateral";
@@ -8,8 +9,9 @@ interface LayoutProps{
     children?: any
 }
 export default function Layout(props: LayoutProps){
+    const {tema} = useAppData()
     return (
-        <div className={`dark flex h-screen w-screen`}>
+        <div className={`${tema} flex h-screen w-screen`}>
             <MenuLateral/>
             <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
             <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo}/>
--- my-project/pages/notificacoes.tsx
+++ my-project/pages/notificacoes.tsx
@@ -2,10 +2,11 @@ import Layout from "../components/template/Layout";
 import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
- const ctx = useAppData()
+ const {alternarTema} = useAppData()
   return (
     <Layout titulo={"Notificações"} subtitulo={"Aqui você ira gerenciar suas notificações"}>
-      <h3>{ctx.nome}</h3>
+      
+      <button onClick={alternarTema}>Alternar Tema</button>
 
     </Layout>
   
