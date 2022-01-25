82159f3b0b95188f10c591d9d83f4f0da6ebe33c
Radames Ribeiro de Sousa <easycompvila@gmail.com>
2022-01-25 18:43:28 -0300

    Implementação inicial de Context Api

--- my-project/pages/notificacoes.tsx
+++ my-project/pages/notificacoes.tsx
@@ -1,9 +1,11 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+ const ctx = useAppData()
   return (
     <Layout titulo={"Notificações"} subtitulo={"Aqui você ira gerenciar suas notificações"}>
-      <h3 className={``}>Conteudo</h3>
+      <h3>{ctx.nome}</h3>
 
     </Layout>
   
