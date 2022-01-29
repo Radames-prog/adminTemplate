3998235d2777c902f3eca9cb51bd47452687f2d4
Radames Ribeiro de Sousa <easycompvila@gmail.com>
2022-01-29 19:43:34 -0300

    Implementação de mensgem de erro

--- my-project/components/icons/index.tsx
+++ my-project/components/icons/index.tsx
@@ -37,3 +37,10 @@ export const InconeLua = (
   <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
 </svg>
 )
+
+
+export const InconeAtencao = (tamanho =6)=>(
+<svg xmlns="http://www.w3.org/2000/svg" className={`h-${tamanho} w-${tamanho}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+</svg>
+)
